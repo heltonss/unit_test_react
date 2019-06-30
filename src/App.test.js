8b2bbcb6e4393ca32a399ec890cdccd985b2f39b
Heltonss <Helton@heltonsouza.com.br>
2019-06-30 10:05:19 -0300

    first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { mount } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('renders without crashing hello world', () => {
+  const wrapper = mount(<App />);
+  expect(wrapper.find('h1').exists()).toBe(true);
+  expect(wrapper.contains(<h1>Hello World</h1>)).toBe(true);
 });
